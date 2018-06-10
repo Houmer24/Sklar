2e668012935545ec70c3cda1bc366b89fec3ab52
Max Lab <88008maxa@gmail.com>
2018-06-10 14:16:12 +0300

    ^C

--- sayhello.php
+++ sayhello.php
@@ -9,12 +9,12 @@ if ($_POST['user']) {
 	print "!";
 } else {
 	//иначе - вывести на экран саму форму
-	print <<<_HTML_
-	<form method="post" action="$_SERVER[PHP_SELF]">
-	Your Name: <input type="text" name="user"/>
-	</br>
-	<button type="submit">Say Hello</button>
-	</form>
-	_HTML_;
+print <<<_HTML_
+<form method="post" action="$_SERVER[PHP_SELF]">
+Your Name: <input type="text" name="user"/>
+</br>
+<button type="submit">Say Hello</button>
+</form>
+_HTML_;//Обязательно с начала строки!!!
 }
 ?>
